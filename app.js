c46ec8cbacdd559a833aa197073d5e5d34e9294f
Rikki407 <rishablamba407@gmail.com>
2018-02-12 00:13:40 +0530

    Completed

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var app  = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+var friends = ["Rishab","Rajat","Snehil","Lokesh"];
+
 
 app.get("/",function (req, res) {
     res.render("home");
 });
-
 app.get("/friends",function (req, res) {
-    var friends = ["Rishab","Rajat","Snehil","Lokesh"];
   res.render("friends",{friends : friends});
 });
 
-
+app.post("/addFriends",function (req, res) {
+    var newFriend = req.body.new_name;
+    friends.push(newFriend);
+    // res.send("You have reached the POST route");
+    res.redirect("/friends")
+});
 
 
 app.listen(3000,function () {
